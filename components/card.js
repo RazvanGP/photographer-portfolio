495a92861b9a4ad9bd769a57b53b2f5458d3b37c
Razvan Plingu <plingu.razvan@gmail.com>
2023-04-11 18:09:48 +0300

    Project set up

---
+++ components/card.js
@@ -0,0 +1,10 @@
+import styles from "./styles/card.module.css";
+
+const Card = () => {
+    return(
+        <div className={styles.cardWrapper}>
+            kjasdihbasd
+        </div>
+    )
+}
+export default Card;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import Layout from '../components/layout';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap");
+  }, []);
+
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
