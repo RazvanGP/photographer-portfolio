7f9b6dd2b0ef3e20568a4506c26076c1cd71007f
Razvan Plingu <plingu.razvan@gmail.com>
2023-04-18 22:24:15 +0300

    small update

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,9 +1,10 @@
+import styles from "../../styles/event.module.css";
+import Image from "next/image";
 import { useRouter } from "next/router";
 
 const Event = () => {
   const router = useRouter();
   const eventId = router.query.eventId;
-
   return (
     <div className="absolute top-1/2 left-1/2 text-white">Event: {eventId}</div>
   );
--- pages/events/yourEvent.js
+++ pages/events/yourEvent.js
@@ -7,24 +7,14 @@ const YourEvent = () => {
   const { data, error, isLoading } = useSWR("../api/staticData", fetcher);
 
   //Handle the error state
-  if (error) return <div>Failed to load</div>;
+  if (error) return alert("Failed to load");
   //Handle the loading state
   if (isLoading) return <div>Loading...</div>;
 
   return (
     <div className=" w-full h-screen grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 items-center justify-items-center overflow-auto pt-[180px] pb-[150px] gap-10 bg-gray-950">
       {JSON.parse(data).map((item) => {
-        return (
-          // <Link
-          //   className="w-[300px] h-[500px] border-2"
-          //   href={{
-          //     pathname: `./${item.name}`,
-          //   }}
-          // >
-          //   Go to pages/yourEvent/{item.name}.js
-          // </Link>
-          <EventCard item={item} key={item.id}></EventCard>
-        );
+        return <EventCard item={item} key={item.id}></EventCard>;
       })}
     </div>
   );
